d35ad8225821b89317658ac3ca63bc76c1756516
zqqiang <qiangzhaoqing@gmail.com>
2017-06-11 21:04:43 -0700

    add basic demo

--- src/kotlin/demo/src/main/kotlin/demo/app.kt
+++ src/kotlin/demo/src/main/kotlin/demo/app.kt
@@ -8,6 +8,32 @@ fun getGreeting(): String {
     return words.joinToString(separator = " ")
 }
 
+fun loop() {
+    val items = listOf("apple", "banana", "kiwi")
+    for (item in items) {
+        println(item)
+    }
+    for (index in items.indices) {
+        println("item at $index is ${items[index]}")
+    }
+}
+
+fun describe(obj: Any): String = 
+when (obj) {
+    1 -> "One"
+    "Hello" -> "Greeting"
+    is Long -> "Long"
+    !is String -> "Not a String"
+    else -> "Unknown"
+}
+
 fun main(args: Array<String>) {
     println(getGreeting())
+
+    loop();
+    println(describe(1));
+    println(describe("Hello"));
+    println(describe(100));
+    println(describe(true));
+    
 }
