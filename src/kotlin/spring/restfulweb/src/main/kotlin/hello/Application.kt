6c189f6e3b5f9ee98dbb0959dc732090e7bbe98f
zqqiang <qiangzhaoqing@gmail.com>
2017-06-23 09:41:12 -0700

    update kt

--- src/kotlin/spring/restfulweb/src/main/kotlin/hello/Application.kt
+++ src/kotlin/spring/restfulweb/src/main/kotlin/hello/Application.kt
@@ -1,34 +1,38 @@
 package org.jetbrains.kotlin.hello
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-open class Application {
+class Application {
+
+    @Bean
+    fun restTemplate(builder: RestTemplateBuilder): RestTemplate {
+        return builder.build()
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun run(restTemplate: RestTemplate): CommandLineRunner {
+        return {
+            val quote = restTemplate.getForObject(
+                    "http://gturnquist-quoters.cfapps.io/api/random", Quote::class.java)
+            log.info(quote.toString())
+        }
+    }
+
     companion object {
-        val log = LoggerFactory.getLogger(Application::class.java);
+
+        private val log = LoggerFactory.getLogger(Application::class.java)
 
         @JvmStatic fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
-        }
-
-        @Bean
-        fun restTemplate(builder: RestTemplateBuilder) {
-            return builder.build();
-        }
-
-        @Bean
-        fun run(restTemplate: RestTemplate) {
-            return (args) -> {
-                var quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote::class);
-                log.info(quote.toString());
-            };
+            SpringApplication.run(Application::class.java)
         }
     }
 }
