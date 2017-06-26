b0702685e87fcf3074a147d865f49cb49cfb078c
zqqiang <qiangzhaoqing@gmail.com>
2017-06-26 09:02:21 -0700

    more kotlin

--- src/kotlin/demo/src/main/kotlin/demo/app.kt
+++ src/kotlin/demo/src/main/kotlin/demo/app.kt
@@ -42,10 +42,33 @@ fun testRange() {
     }
 }
 
+interface Clickable {
+    fun click()
+    fun showOff() = println("I'm clickable!")
+}
+
+interface Focusable {
+    fun setFocus(b: Boolean) = println("I ${if (b) "got" else "lost"} focus.")
+    fun showOff() = println("I'm focusable!")
+}
+
+class Button : Clickable, Focusable {
+    override fun click() = println("I was clicked")
+    override fun showOff() {
+        super<Clickable>.showOff()
+        super<Focusable>.showOff()
+    }
+}
+
 fun main(args: Array<String>) {
     println(getGreeting())
 
-    loop();
-    testWhen();
-    testRange();
+    loop()
+    testWhen()
+    testRange()
+
+    val button = Button()
+    button.showOff()
+    button.setFocus(true)
+    button.click()
 }
