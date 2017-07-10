7b686a4cb5a1499cf79de8d1b84d74de0221b097
zqqiang <qiangzhaoqing@gmail.com>
2017-07-10 09:14:09 -0700

    add sealed class

--- src/kotlin/demo/src/main/kotlin/demo/app.kt
+++ src/kotlin/demo/src/main/kotlin/demo/app.kt
@@ -102,6 +102,18 @@ fun TalkativeButton.giveSpeech() {
 }
 */
 
+// sealed class
+sealed class Expr {
+    class Num(val value: Int) : Expr()
+    class Sum(val left: Expr, val right: Expr) : Expr()
+}
+
+fun eval(e: Expr) : Int = 
+    when (e) {
+        is Expr.Num -> e.value
+        is Expr.Sum -> eval(e.right) + eval(e.left)
+    }
+
 fun main(args: Array<String>) {
     println(getGreeting())
 
