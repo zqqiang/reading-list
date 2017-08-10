5bcc625f4ea7dd419c0c278410ec6131178e283d
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2017-08-10 08:52:47 -0700

    add gwt history

--- src/java/gwtDocs/src/com/google/gwt/sample/docs/client/GwtDocs.java
+++ src/java/gwtDocs/src/com/google/gwt/sample/docs/client/GwtDocs.java
@@ -8,14 +8,20 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -34,6 +40,8 @@ public class GwtDocs implements EntryPoint {
 	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 	
 	private final CustomServiceAsync customService = GWT.create(CustomService.class);
+	
+	TabPanel tabPanel;
 
 	/**
 	 * This is the entry point method.
@@ -170,5 +178,36 @@ public class GwtDocs implements EntryPoint {
 		MyHandler handler = new MyHandler();
 		sendButton.addClickHandler(handler);
 		nameField.addKeyUpHandler(handler);
+		
+		tabPanel = new TabPanel();
+		tabPanel.add(new HTML("<h1>Page 0 Content: Llamas</h1>"), " Page 0 ");
+	    tabPanel.add(new HTML("<h1>Page 1 Content: Alpacas</h1>"), " Page 1 ");
+	    tabPanel.add(new HTML("<h1>Page 2 Content: Camels</h1>"), " Page 2 ");
+	    
+	    tabPanel.addSelectionHandler(new SelectionHandler<Integer>() {
+	    	public void onSelection(SelectionEvent<Integer> event) {
+	    		History.newItem("page" + event.getSelectedItem());
+	    	}
+	    });
+	    
+	    History.addValueChangeHandler(new ValueChangeHandler<String>() {
+	    	public void onValueChange(ValueChangeEvent<String> event) {
+	    		String historyToken = event.getValue();
+	    		try {
+	    			if (historyToken.substring(0,  4).equals("page")) {
+	    				String tabIndexToken = historyToken.substring(4,  5);
+	    				int tabIndex = Integer.parseInt(tabIndexToken);
+	    				tabPanel.selectTab(tabIndex);
+	    			} else {
+	    				tabPanel.selectTab(0);
+	    			}
+	    		} catch (IndexOutOfBoundsException e) {
+	    			tabPanel.selectTab(0);
+	    		}
+	    	} 
+	    });
+
+	    tabPanel.selectTab(0);
+	    RootPanel.get("tabContainer").add(tabPanel);
 	}
 }
