9d92efea75c4cd5de52623bf3a74db155f735d89
zqqiang <qiangzhaoqing@gmail.com>
2018-01-09 08:30:21 -0800

    update go sublime

--- src/go/codewalk/json.go
+++ src/go/codewalk/json.go
@@ -1,115 +1,115 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "os"
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
 type Message struct {
-    Name string
-    Body string
-    Time int64
+	Name string
+	Body string
+	Time int64
 }
 
 func encoding() []byte {
-    m := Message{"Alice", "Hello", 1294706395881547000}
-    b, err := json.Marshal(m)
-    if err != nil {
-        fmt.Println(err.Error())
-        return b
-    }
-    fmt.Println(string(b))
-    return b
+	m := Message{"Alice", "Hello", 1294706395881547000}
+	b, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println(err.Error())
+		return b
+	}
+	fmt.Println(string(b))
+	return b
 }
 
 func decoding(b []byte) {
-    var m Message
-    err := json.Unmarshal(b, &m)
-    if err != nil {
-        fmt.Println(err.Error())
-    }
-    fmt.Println(m)
+	var m Message
+	err := json.Unmarshal(b, &m)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(m)
 }
 
 func generic() {
-    var i interface{}
-    i = "a string"
-    i = 2011
-    i = 2.777
+	var i interface{}
+	i = "a string"
+	i = 2011
+	i = 2.777
 
-    r := i.(float64)
-    fmt.Println(r)
+	r := i.(float64)
+	fmt.Println(r)
 
-    switch v := i.(type) {
-    case int:
-        fmt.Println("int", v)
-    case float64:
-        fmt.Println("float64", v)
-    case string:
-        fmt.Println("string", v)
-    default:
-        fmt.Println("unknown type")
-    }
+	switch v := i.(type) {
+	case int:
+		fmt.Println("int", v)
+	case float64:
+		fmt.Println("float64", v)
+	case string:
+		fmt.Println("string", v)
+	default:
+		fmt.Println("unknown type")
+	}
 }
 
 func arbitray() {
-    b := []byte(`{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"]}`)
-    var f interface{}
-    err := json.Unmarshal(b, &f)
-    if err != nil {
-        fmt.Println(err.Error())
-        return
-    }
-    m := f.(map[string]interface{})
-    fmt.Println(m)
+	b := []byte(`{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"]}`)
+	var f interface{}
+	err := json.Unmarshal(b, &f)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	m := f.(map[string]interface{})
+	fmt.Println(m)
 
-    for k, v := range m {
-        switch vv := v.(type) {
-        case string:
-            fmt.Println(k, "is string", vv)
-        case float64:
-            fmt.Println(k, "is float64", vv)
-        case []interface{}:
-            fmt.Println(k, "is an array:")
-            for i, u := range vv {
-                fmt.Println(i, u)
-            }
-        default:
-            fmt.Println(k, "unknown type")
-        }
-    }
+	for k, v := range m {
+		switch vv := v.(type) {
+		case string:
+			fmt.Println(k, "is string", vv)
+		case float64:
+			fmt.Println(k, "is float64", vv)
+		case []interface{}:
+			fmt.Println(k, "is an array:")
+			for i, u := range vv {
+				fmt.Println(i, u)
+			}
+		default:
+			fmt.Println(k, "unknown type")
+		}
+	}
 }
 
 func stream() {
-    dec := json.NewDecoder(os.Stdin)
-    enc := json.NewEncoder(os.Stdout)
-    for {
-        var v map[string]interface{}
-        if err := dec.Decode(&v); err != nil {
-            fmt.Println(err.Error())
-            return
-        }
-        for k := range v {
-            if k != "Name" {
-                delete(v, k)
-            }
-        }
-        if err := enc.Encode(&v); err != nil {
-            fmt.Println(err.Error())
-        }
-    }
+	dec := json.NewDecoder(os.Stdin)
+	enc := json.NewEncoder(os.Stdout)
+	for {
+		var v map[string]interface{}
+		if err := dec.Decode(&v); err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		for k := range v {
+			if k != "Name" {
+				delete(v, k)
+			}
+		}
+		if err := enc.Encode(&v); err != nil {
+			fmt.Println(err.Error())
+		}
+	}
 }
 
 func main() {
 
-    b := encoding()
+	b := encoding()
 
-    decoding(b)
+	decoding(b)
 
-    generic()
+	generic()
 
-    arbitray()
+	arbitray()
 
-    stream()
+	stream()
 }
--- src/go/codewalk/urlpoll.go
+++ src/go/codewalk/urlpoll.go
@@ -1,98 +1,98 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "time"
+	"log"
+	"net/http"
+	"time"
 )
 
 const (
-    numPollers = 2
-    pollInterval = 60 * time.Second
-    statusInterval = 10 * time.Second
-    errTimeout = 10 * time.Second
+	numPollers     = 2
+	pollInterval   = 60 * time.Second
+	statusInterval = 10 * time.Second
+	errTimeout     = 10 * time.Second
 )
 
 var urls = []string{
-    "http://www.google.com/",
-    "http://golang.com/",
-    "http://blog.golang.org/",
+	"http://www.google.com/",
+	"http://golang.com/",
+	"http://blog.golang.org/",
 }
 
 type State struct {
-    url string
-    status string
+	url    string
+	status string
 }
 
 func StateMonitor(updateInterval time.Duration) chan<- State {
-    updates := make(chan State)
-    urlStates := make(map[string]string)
-    ticker := time.NewTicker(updateInterval)
-    go func() {
-        for {
-            select {
-            case <- ticker.C:
-                logState(urlStates)
-            case s := <-updates:
-                urlStates[s.url] = s.status
-            }
-        }
-    }()
-    return updates
+	updates := make(chan State)
+	urlStates := make(map[string]string)
+	ticker := time.NewTicker(updateInterval)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				logState(urlStates)
+			case s := <-updates:
+				urlStates[s.url] = s.status
+			}
+		}
+	}()
+	return updates
 }
 
 func logState(s map[string]string) {
-    log.Println("Current state:")
-    for k,v := range s {
-        log.Printf(" %s %s", k, v)
-    }
+	log.Println("Current state:")
+	for k, v := range s {
+		log.Printf(" %s %s", k, v)
+	}
 }
 
 type Resource struct {
-    url string
-    errCount int
+	url      string
+	errCount int
 }
 
 func (r *Resource) Poll() string {
-    resp, err := http.Head(r.url)
-    if err != nil {
-        log.Println("Error", r.url, err)
-        r.errCount++
-        return err.Error()
-    }
-    r.errCount = 0
-    return resp.Status
+	resp, err := http.Head(r.url)
+	if err != nil {
+		log.Println("Error", r.url, err)
+		r.errCount++
+		return err.Error()
+	}
+	r.errCount = 0
+	return resp.Status
 }
 
 func (r *Resource) Sleep(done chan<- *Resource) {
-    time.Sleep(pollInterval + errTimeout*time.Duration(r.errCount))
-    done <- r
+	time.Sleep(pollInterval + errTimeout*time.Duration(r.errCount))
+	done <- r
 }
 
 func Poller(in <-chan *Resource, out chan<- *Resource, status chan<- State) {
-    for r := range in {
-        s := r.Poll()
-        status <- State{r.url, s}
-        out <- r
-    }
+	for r := range in {
+		s := r.Poll()
+		status <- State{r.url, s}
+		out <- r
+	}
 }
 
 func main() {
-    pending, complete := make(chan *Resource), make(chan *Resource)
+	pending, complete := make(chan *Resource), make(chan *Resource)
 
-    status := StateMonitor(statusInterval)
+	status := StateMonitor(statusInterval)
 
-    for i := 0; i < numPollers; i++ {
-        go Poller(pending, complete, status)
-    }
+	for i := 0; i < numPollers; i++ {
+		go Poller(pending, complete, status)
+	}
 
-    go func() {
-        for _,url := range urls {
-            pending <- &Resource{url: url}
-        }
-    }()
+	go func() {
+		for _, url := range urls {
+			pending <- &Resource{url: url}
+		}
+	}()
 
-    for r := range complete {
-        go r.Sleep(pending)
-    }
+	for r := range complete {
+		go r.Sleep(pending)
+	}
 }
