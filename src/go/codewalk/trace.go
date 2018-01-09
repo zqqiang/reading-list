be95daeefb14e03ab9953566c819055deabbfa92
zqqiang <qiangzhaoqing@gmail.com>
2018-01-08 20:54:00 -0800

    add trace go

---
+++ src/go/codewalk/trace.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptrace"
+)
+
+type transport struct {
+	current *http.Request
+}
+
+func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	t.current = req
+	return http.DefaultTransport.RoundTrip(req)
+}
+
+func (t *transport) GotConn(info httptrace.GotConnInfo) {
+	fmt.Printf("Connection reused for %v? %v\n", t.current.URL, info.Reused)
+}
+
+func trace() {
+	req, _ := http.NewRequest("GET", "https://www.google.com", nil)
+	trace := &httptrace.ClientTrace{
+		DNSDone: func(dnsInfo httptrace.DNSDoneInfo) {
+			fmt.Printf("DNS Info: %+v\n", dnsInfo)
+		},
+		GotConn: func(connInfo httptrace.GotConnInfo) {
+			fmt.Printf("Got Conn: %+v\n", connInfo)
+		},
+	}
+	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+	if _, err := http.DefaultTransport.RoundTrip(req); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func trace2() {
+	t := &transport{}
+
+	req, _ := http.NewRequest("GET", "https://google.com", nil)
+	trace := &httptrace.ClientTrace{
+		GotConn: t.GotConn,
+	}
+	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+
+	client := &http.Client{Transport: t}
+	if _, err := client.Do(req); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	trace2()
+}
