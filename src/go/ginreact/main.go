c1f2ba0705f0aa143529a5b3338a0a16f2c2a0f4
zqqiang <qiangzhaoqing@gmail.com>
2018-01-11 08:56:26 -0800

    gin react demo work

--- src/go/ginreact/main.go
+++ src/go/ginreact/main.go
@@ -1,48 +1,15 @@
 package main
 
 import (
-	"os"
-
-	"net/http"
-	"strings"
-
-	"github.com/elazarl/go-bindata-assetfs"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"os"
 )
-
-type binaryFileSystem struct {
-	fs http.FileSystem
-}
-
-func (b *binaryFileSystem) Open(name string) (http.File, error) {
-	return b.fs.Open(name)
-}
-
-func (b *binaryFileSystem) Exists(prefix string, filepath string) bool {
-	if p := strings.TrimPrefix(filepath, prefix); len(p) < len(filepath) {
-		if _, err := b.fs.Open(p); err != nil {
-			return false
-		}
-		return true
-	}
-	return false
-}
-
-func BinaryFileSystem(root string) *binaryFileSystem {
-	fs := &assetfs.AssetFS{Asset, AssetDir, AssetInfo, root}
-	return &binaryFileSystem{
-		fs,
-	}
-}
 
 func main() {
 	r := gin.Default()
 
-	// We can't use router.Static method to use '/' for static files.
-	// see https://github.com/gin-gonic/gin/issues/75
-	// r.StaticFS("/", assetFS())
-	r.Use(static.Serve("/", BinaryFileSystem("public")))
+	r.Use(static.Serve("/", static.LocalFile("./public", true)))
 
 	// add routes
 	// r.GET("/api/home", controllers.Home)
