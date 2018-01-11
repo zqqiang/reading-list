245bb9842d950ad912426444c9d20253d13ca455
zqqiang <qiangzhaoqing@gmail.com>
2018-01-11 13:58:51 -0800

    udpate webpack config

--- src/go/ginreact/webpack.config.js
+++ src/go/ginreact/webpack.config.js
@@ -1,16 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    entry: './public/main.js',
+    entry: [
+        './public/main.js'
+    ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
     },
     module: {
-        loaders: [{
-            test: /\.js(x?)$/,
-            exclude: [/node_modules/],
-            loader: 'babel-loader?presets[]=es2015&presets[]=react'
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015', 'react']
+                }
+            }
         }]
-    },
+    }
 };
