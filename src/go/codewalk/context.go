bb99d9501369c73d1b4a29faee430af678c427f6
zqqiang <qiangzhaoqing@gmail.com>
2018-01-16 07:49:18 -0800

    go context done

--- src/go/codewalk/context.go
+++ src/go/codewalk/context.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-    "fmt"
+	"context"
+	"fmt"
 )
 
+func gen(ctx context.Context) <-chan int {
+	ch := make(chan int)
+	go func() {
+		var n int
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case ch <- n:
+				n++
+			}
+		}
+	}()
+	return ch
+}
+
+func test_leak_goroutine() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for n := range gen(ctx) {
+		fmt.Println(n)
+		if n == 5 {
+			cancel()
+			break
+		}
+	}
+}
+
 func main() {
-    
+	test_leak_goroutine()
 }
