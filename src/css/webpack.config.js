b8377b778d76d6f83cc330ed753ceb52e909fa58
zqqiang <qiangzhaoqing@gmail.com>
2018-02-04 12:49:58 -0800

    try sass webpack config

--- src/css/webpack.config.js
+++ src/css/webpack.config.js
@@ -4,7 +4,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     entry: [
         './public/main.js',
-        // './public/themes/sass/main.scss'
+        './public/themes/sass/main.scss'
     ],
     output: {
         filename: 'bundle.js',
@@ -25,17 +25,17 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [{
-                    loader: "style-loader" // creates style nodes from JS strings
+                    loader: "style-loader"
                 }, {
-                    loader: "css-loader" // translates CSS into CommonJS
+                    loader: "css-loader"
                 }, {
-                    loader: "sass-loader" // compiles Sass to CSS
+                    loader: "sass-loader"
                 }]
             }
         ]
     },
     plugins: [
-        new ExtractTextPlugin({ // define where to save the file
+        new ExtractTextPlugin({
             filename: './public/themes/css/main.css',
             allChunks: true,
         }),
