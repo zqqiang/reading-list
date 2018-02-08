8d451e7468f46d855ef289bd6c5ab4e219987fe0
zqqiang <qiangzhaoqing@gmail.com>
2018-02-08 09:07:17 -0800

    update web

--- src/web/public/components/app.js
+++ src/web/public/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+const wrapperStyle = { height: "auto", minHeight: "100%" }
+
 export default class App extends React.Component {
     render() {
         return (
-            <div className="wrapper" style={{height: "auto", minHeight: "100%"}}>
+            <div className="wrapper" style={wrapperStyle}>
 
             </div>
         )
