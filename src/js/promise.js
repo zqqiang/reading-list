cbb9caa6c23f664628232561cc5a839c23cd27ed
zqqiang <qiangzhaoqing@gmail.com>
2018-06-24 11:31:51 -0700

    update promise

--- src/js/promise.js
+++ src/js/promise.js
@@ -1,2 +1,23 @@
 'use strict';
 
+(() => {
+    // basic usage
+    console.log("Started");
+
+    let myPromise = new Promise((resolve, reject) => {
+        console.log("Promise started!");
+
+        setTimeout(function () {
+            resolve("Success!");
+        }, 1000);
+    });
+
+    myPromise.then((successMessage) => {
+        console.log("Promise fullfilled!");
+        console.log("Yah, " + successMessage);
+    }).catch(() => {
+        console.log("Promise rejected!");
+    });
+
+    console.log("Promise made");
+})();
