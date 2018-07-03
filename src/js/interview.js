7eb17a66e93cbd686a0d492c5b8ee0378194432c
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2018-07-03 16:51:00 -0700

    add js symbol

--- src/js/interview.js
+++ src/js/interview.js
@@ -96,3 +96,25 @@
         .reduce((acc, key) => (acc[key] = obj[key], acc), {});
     console.log(deepReduceClone);
 })();
+
+(() => {
+    Symbol("foo") !== Symbol("foo")
+    const foo = Symbol()
+    const bar = Symbol()
+    typeof foo === "symbol"
+    typeof bar === "symbol"
+
+    console.log(Symbol.for("foo"))
+    console.log(Symbol.keyFor(Symbol.for("foo")))
+
+    let obj = {}
+    obj[foo] = "foo"
+    obj[bar] = "bar"
+    console.log(obj[foo])
+    console.log(obj[bar])
+
+    console.log(JSON.stringify(obj)) // {}
+    console.log(Object.keys(obj)) // []
+    console.log(Object.getOwnPropertyNames(obj)) // []
+    console.log(Object.getOwnPropertySymbols(obj)) // [ Symbol(), Symbol() ]
+})();
