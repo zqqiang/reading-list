75615c679f99523d53367772d329e61672ceb86d
zqqiang <qiangzhaoqing@gmail.com>
2018-09-12 20:13:17 -0700

    update schedule

--- doc/schedule/schedule_2018.md
+++ doc/schedule/schedule_2018.md
@@ -55,7 +55,7 @@
 1. [libuv](http://libuv.org/) - [dns_start]()
 1. [Understanding the Linux Kernel]() - [p28:计时]()
 1. [css-sprites](https://css-tricks.com/css-sprites/) - [Todos]()
-1. [Secure Coding](https://www.securecoding.cert.org/confluence/display/c/SEI+CERT+C+Coding+Standard) - [PRE31-C. Avoid side effects in arguments to unsafe macros]()
+1. [Secure Coding](https://www.securecoding.cert.org/confluence/display/c/SEI+CERT+C+Coding+Standard) - [Rule 02: DCL36-C]()
 1. [Modern C]() - [p21: Acquaintance/p254:Improve type generic expression programming]()
 1. [og-aws](https://github.com/open-guides/og-aws) - [S3]()
 1. [python crash course]() - []()
--- src/c/seicert.c
+++ src/c/seicert.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include "seicert.h"
 
-static inline short sabs(short v) {
+static inline short sabs(short v)
+{
 	printf("short abs\n");
 	return v < 0 ? -v : v;
 }
 
-static inline int iabs(int v) {
+static inline int iabs(int v)
+{
 	printf("int abs\n");
 	return v < 0 ? -v : v;
-} 
+}
 
-#define ABS(v) _Generic(v, short : sabs, int : iabs)(v)
+#define ABS(v) _Generic(v, short    \
+						: sabs, int \
+						: iabs)(v)
 
-int main(int argc, char const *argv[]) {
+static int unsafe_macros()
+{
+	printf("PRE31-C. Avoid side effects in arguments to unsafe macros\n");
+
 	int i = 10;
 	short s = 5;
 	ABS(i);
 	ABS(s);
+	return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+	unsafe_macros();
+
 	return 0;
 }
