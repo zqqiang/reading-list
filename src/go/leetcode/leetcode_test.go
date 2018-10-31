1f5603b368e3676ce5b706a9dcad35295d639600
zqqiang <qiangzhaoqing@gmail.com>
2018-10-31 09:36:16 -0700

    update leetcode

---
+++ src/go/leetcode/leetcode_test.go
@@ -0,0 +1,77 @@
+package leetcode
+
+/*
+2. Add Two Numbers
+
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order and each of their nodes contain a single digit.
+Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var sum int
+	var incr int
+	var last *ListNode
+	var head *ListNode
+	var cur *ListNode
+
+	if l1 != nil && l2 != nil {
+		sum = l1.Val + l2.Val
+		incr = sum / 10
+
+		cur = &ListNode{sum % 10, nil}
+		head = cur
+		last = cur
+
+		l1 = l1.Next
+		l2 = l2.Next
+		for l1 != nil || l2 != nil || incr != 0 {
+			if l1 != nil && l2 != nil {
+				sum = l1.Val + l2.Val + incr
+				cur = &ListNode{sum % 10, nil}
+				incr = sum / 10
+				l1 = l1.Next
+				l2 = l2.Next
+			} else if l1 != nil && l2 == nil {
+				sum = l1.Val + incr
+				cur = &ListNode{sum % 10, nil}
+				incr = sum / 10
+				l1 = l1.Next
+			} else if l1 == nil && l2 != nil {
+				sum = l2.Val + incr
+				cur = &ListNode{sum % 10, nil}
+				incr = sum / 10
+				l2 = l2.Next
+			} else if incr != 0 {
+				cur = &ListNode{incr % 10, nil}
+				incr = 0
+			}
+			last.Next = cur
+			last = cur
+		}
+		return head
+	}
+
+	return nil
+}
