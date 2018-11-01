02abdff52ffbdebe8be87a9dfa1463c02a4617ba
zqqiang <qiangzhaoqing@gmail.com>
2018-11-01 09:45:43 -0700

    update leetcode

--- src/go/leetcode/leetcode_test.go
+++ src/go/leetcode/leetcode_test.go
@@ -75,3 +75,30 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	return nil
 }
+
+/*
+3. Longest Substring Without Repeating Characters
+
+Given a string, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+Example 2:
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. 
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+*/
+
+func lengthOfLongestSubstring(s string) int {
+    return 0
+}
