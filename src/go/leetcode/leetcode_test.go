5d8cd63f95bd31a47e7a5df0fea8d0d7f521e3b1
zqqiang <qiangzhaoqing@gmail.com>
2018-11-05 09:20:53 -0800

    update leetcode

--- src/go/leetcode/leetcode_test.go
+++ src/go/leetcode/leetcode_test.go
@@ -1,5 +1,10 @@
 package leetcode
 
+import (
+	"fmt"
+	"strings"
+)
+
 /*
 2. Add Two Numbers
 
@@ -178,4 +183,46 @@ func longestPalindrome(s string) string {
 		}
 	}
 	return s[maxStart : maxEnd+1]
+}
+
+/*
+6. ZigZag Conversion
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+*/
+
+func convert(s string, numRows int) string {
+	var row int
+	var col int
+	var result strings.Builder
+
+	for i := 0; i < len(s); i++ {
+		row = i / numRows
+		col = i % numRows
+		fmt.Fprintf(&result, "%d", i)
+	}
+
+	return result.String()
 }
