af302f2382accc8ea471c21ad63e5a7fb3306c08
zqqiang <qiangzhaoqing@gmail.com>
2018-11-10 09:21:24 -0800

    update leetcode

--- src/go/leetcode/leetcode_test.go
+++ src/go/leetcode/leetcode_test.go
@@ -2,6 +2,7 @@ package leetcode
 
 import (
 	"bytes"
+	"math"
 )
 
 /*
@@ -244,4 +245,38 @@ func convert(s string, numRows int) string {
 		}
 	}
 	return buffer.String()
+}
+
+/*
+7. Reverse Integer
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+*/
+
+func reverse(x int) int {
+	var result int
+	var step uint
+	for ; x != 0; step += 4 {
+		m := (x % 10)
+		x = x / 10
+		result = (result*10 + m)
+	}
+	if result > (int(math.Pow(2, 31))-1) || result < -int(math.Pow(2, 31)) {
+		return 0
+	}
+	return result
 }
