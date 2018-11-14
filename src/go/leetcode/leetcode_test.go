0ddc9e45cd4a2b9cfa5c563c9e578b0ef0064380
zqqiang <qiangzhaoqing@gmail.com>
2018-11-13 21:04:44 -0800

    update leetcode

--- src/go/leetcode/leetcode_test.go
+++ src/go/leetcode/leetcode_test.go
@@ -280,3 +280,90 @@ func reverse(x int) int {
 	}
 	return result
 }
+
+/*
+8. String to Integer (atoi)
+Implement atoi which converts a string to an integer.
+
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+
+If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned.
+
+Note:
+
+Only the space character ' ' is considered as whitespace character.
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
+Example 1:
+
+Input: "42"
+Output: 42
+Example 2:
+
+Input: "   -42"
+Output: -42
+Explanation: The first non-whitespace character is '-', which is the minus sign.
+             Then take as many numerical digits as possible, which gets 42.
+Example 3:
+
+Input: "4193 with words"
+Output: 4193
+Explanation: Conversion stops at digit '3' as the next character is not a numerical digit.
+Example 4:
+
+Input: "words and 987"
+Output: 0
+Explanation: The first non-whitespace character is 'w', which is not a numerical
+             digit or a +/- sign. Therefore no valid conversion could be performed.
+Example 5:
+
+Input: "-91283472332"
+Output: -2147483648
+Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
+             Thefore INT_MIN (−231) is returned.
+*/
+func myAtoi(str string) int {
+	// todo: not working
+	var sign byte
+	var result int
+	var step int
+
+	const (
+		MaxInt32 = 1<<31 - 1
+		MinInt32 = -1 << 31
+	)
+
+	for i := 0; i < len(str); i++ {
+		if str[i] == '+' || str[i] == '-' {
+			sign = str[i]
+		} else if str[i] >= 0 && str[i] <= 9 {
+			if step == 0 {
+				result += (int(str[i]) - '0')
+			} else {
+				result += (int(str[i]) - '0') * step * 10
+			}
+			step++
+		} else if str[i] == ' ' {
+
+		} else if step == 0 {
+			return 0
+		} else if step != 0 {
+			break
+		}
+	}
+
+	if sign == '-' {
+		result *= -1
+	}
+
+	if result > MaxInt32 {
+		return MaxInt32
+	} else if result < MinInt32 {
+		return MinInt32
+	} else {
+		return result
+	}
+}
