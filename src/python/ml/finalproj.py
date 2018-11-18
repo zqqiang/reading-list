e3e0129a3c7602a4ffebd0dcdc99227e1bee1c70
zqqiang <qiangzhaoqing@gmail.com>
2018-11-18 14:46:02 -0800

    update ml py

--- src/python/ml/finalproj.py
+++ src/python/ml/finalproj.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 from keras.models import Sequential
@@ -20,42 +20,57 @@ def time_series_pre_process(csv):
     csv_data = pd.read_csv(csv,
                            parse_dates=[['year', 'month', 'day', 'hour']],
                            index_col=0)
+
+    print("csv_data after merge date time columns: \n{}".format(csv_data.head(5)))
+
     csv_data['pm2.5'].fillna(0, inplace=True)
     csv_data.drop('No', axis=1, inplace=True)
+
+    print("csv_data after fill N/A and drop No column: \n{}".format(csv_data.head(5)))
 
     return csv_data
 
 
 def time_series_quick_view(time_series):
     """
-    create a time series quick view of each series 
+    create a time series quick view of each series
     """
-    pyplot.figure(figsize=(10, 10))
     plot_index = 1
     cols = ['pm2.5', 'DEWP', 'TEMP', 'PRES', 'Iws', 'Is', 'Ir']
     col_titles = ['pm2.5', 'Dew Point', 'Temperature',
                   'Pressure', 'Cumulated wind speed', 'Cumulated hours of snow', 'Cumulated hours of rain']
+
+    fig, ax = plt.subplots()
+
     for col in cols:
-        pyplot.subplot(len(cols), 1, plot_index)
-        pyplot.plot(time_series[col].values)
-        pyplot.title(col_titles[plot_index-1], y=0.7, loc='right')
+        plt.subplot(len(cols), 1, plot_index)
+        plt.plot(time_series[col].values)
+        plt.title(col_titles[plot_index-1], y=0.8, loc='right')
         plot_index += 1
-    pyplot.show()
+
+    display(fig)
 
 
 def col_label_encode_and_normalize(csv_data, encode_col_index):
     """
-    The wind speed feature is string 
+    The wind speed feature is string
     we need use LabelEncoder to encode it to integer
     use MinMaxScaler to normalize to [0, 1]
     """
     encoded_data = csv_data.values
     encoder = LabelEncoder()
+
+    print("before label encoder:\n{}".format(
+        encoded_data[:, encode_col_index]))
     encoded_data[:, encode_col_index] = encoder.fit_transform(
         encoded_data[:, encode_col_index])
+    print("after label encoder:\n{}".format(encoded_data[:, encode_col_index]))
 
+    print("before normalize:\n{}".format(encoded_data))
     scaler = MinMaxScaler(feature_range=(0, 1))
     scaled_data = scaler.fit_transform(encoded_data)
+    print("after nromalize:\n{}".format(scaled_data))
+
     return scaler, scaled_data
 
 
@@ -85,6 +100,9 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     # drop rows with NaN values
     if dropnan:
         agg.dropna(inplace=True)
+
+    print("supervised datasets:\n{}".format(agg.head()))
+
     return agg
 
 
@@ -96,11 +114,14 @@ def cleanup_supervised_data(supervised_data):
     supervised_data.drop(
         supervised_data.columns[[9, 10, 11, 12, 13, 14, 15]], axis=1, inplace=True)
 
+    print("supervised datasets after cleanup unused column\n{}".format(
+        supervised_data.head()))
+
 
 def split_train_test_datas(datasets, threshold):
     """
     split datasets into train and test sub datasets
-    threshold is the train hours 
+    threshold is the train hours
     """
     train = datasets.values[:threshold, :]
     test = datasets.values[threshold:, :]
@@ -110,8 +131,7 @@ def split_train_test_datas(datasets, threshold):
     # reshape input to be 3D [samples, timesteps, features]
     train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
     test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-    print("train_X.shape:{0}, train_y.shape:{1}, test_X.shape:{2}, test_y.shape:{3}".format(
-        train_X.shape, train_y.shape, test_X.shape, test_y.shape))
+
     return train_X, train_y, test_X, test_y
 
 
@@ -123,7 +143,7 @@ def machine_learning(train_X, train_y, test_X, test_y):
     data_dim = train_X.shape[2]
     timesteps = train_X.shape[1]
     batch_size = 64
-    epochs = 50
+    epochs = 5
 
     print("data_dim:{0}, timesteps:{1}\n".format(data_dim, timesteps))
 
@@ -138,11 +158,10 @@ def machine_learning(train_X, train_y, test_X, test_y):
     model.add(Dense(1))
     # we need to configure the learning process
     # which is done via the compile method
-    # why we choose adam optimizer?
-    #
     model.compile(optimizer='adam', loss='mae')
     # Keras models are trained on Numpy arrays of input data and labels
     # For training a model, you will typically use the  fit function
+    # fit method trains the model for a given number of epochs(iterations on a dataset).
     history = model.fit(train_X, train_y, epochs=epochs, batch_size=batch_size,
                         validation_data=(test_X, test_y), verbose=2, shuffle=False)
     # Its History.history attribute is a record of training
@@ -155,15 +174,19 @@ def history_plot(result):
     Plot training lost values vs metrics values
     If they close enough the epochs is big enough
     """
-    pyplot.figure(figsize=(10, 10))
-    pyplot.plot(result.history['loss'], label='train')
-    pyplot.plot(result.history['val_loss'], label='test')
-    pyplot.legend()
-    pyplot.show()
+    fig, ax = plt.subplots()
+
+    plt.plot(result.history['loss'], label='train')
+    plt.plot(result.history['val_loss'], label='test')
+    plt.legend()
+
+    display(fig)
 
 
 def evaluate_model(model, scaler, test_X, test_y):
     """
+    we calculate the Root Mean Squared Error (RMSE)
+    that gives error in the same units as the variable itself.
     """
     # make a prediction
     y_pred = model.predict(test_X)
@@ -180,26 +203,23 @@ def evaluate_model(model, scaler, test_X, test_y):
     # calculate RMSE
     print("y_true:{0}\ny_pred:{1}\n".format(y_true, y_pred))
     rmse = sqrt(mean_squared_error(y_true, y_pred))
-    print('Test RMSE: %.3f' % rmse)
+    print('Test RMSE: %f' % rmse)
     return rmse
 
 
 def main():
-    pm25_time_series = time_series_pre_process('Beijing.csv')
+    pm25_time_series = time_series_pre_process(
+        '/dbfs/FileStore/tables/Beijing.csv')
     time_series_quick_view(pm25_time_series)
     scaler, encode_normalize_data = col_label_encode_and_normalize(
         pm25_time_series, 4)
     supervised_data = series_to_supervised(encode_normalize_data, 1, 1)
     cleanup_supervised_data(supervised_data)
-    rmse_list = list()
-    for i in range(1, 6):
-        train_X, train_y, test_X, test_y = split_train_test_datas(
-            supervised_data, 365*24*i)
-        model, result = machine_learning(train_X, train_y, test_X, test_y)
-        history_plot(result)
-        rmse_list.append(evaluate_model(model, scaler, test_X, test_y))
-    print(
-        "5-fold cross-validation result: {0}".format(sum(rmse_list)/len(rmse_list)))
+    train_X, train_y, test_X, test_y = split_train_test_datas(
+        supervised_data, 365*24)
+    model, result = machine_learning(train_X, train_y, test_X, test_y)
+    history_plot(result)
+    evaluate_model(model, scaler, test_X, test_y)
 
 
 if __name__ == "__main__":
