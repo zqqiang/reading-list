42f998869d2d6ff535b6bb0f621e1a412773d229
zqqiang <qiangzhaoqing@gmail.com>
2018-12-04 08:18:48 -0800

    update leetcode

--- src/cpp/leetcode/leetcode.cpp
+++ src/cpp/leetcode/leetcode.cpp
@@ -64,3 +64,64 @@ class Solution
         return num;
     }
 };
+/*
+697. Degree of an Array
+Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
+
+Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
+
+Example 1:
+Input: [1, 2, 2, 3, 1]
+Output: 2
+Explanation: 
+The input array has a degree of 2 because both elements 1 and 2 appear twice.
+Of the subarrays that have the same degree:
+[1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+The shortest length is 2. So return 2.
+Example 2:
+Input: [1,2,2,3,1,4,2]
+Output: 6
+Note:
+
+nums.length will be between 1 and 50,000.
+nums[i] will be an integer between 0 and 49,999.
+*/
+
+class Solution
+{
+  public:
+    int findShortestSubArray(vector<int> &nums)
+    {
+        std::map<int, int> left;
+        std::map<int, int> right;
+        std::map<int, int> count;
+
+        for (int i = 0; i < nums.size; ++i)
+        {
+            if (!left[nums[i]])
+            {
+                left[nums[i]] = i;
+            }
+            right[nums[i]] = i;
+            count[nums[i]] += 1;
+        }
+
+        int degree = 0;
+        for (std::map<int, int>::iterator it = count.begin(); it != count.end(); ++it)
+        {
+            if (degree < it->second)
+            {
+                degree = it->second;
+            }
+        }
+
+        int result = 0;
+        for (std::map<int, int>::iterator it = count.begin(); it != count.end(); ++it)
+        {
+            if (it->second == degree)
+            {
+                result = min(result, right[it->second] - left[it->second] + 1);
+            }
+        }
+    }
+};
