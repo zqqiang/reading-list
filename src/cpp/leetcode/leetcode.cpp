d72b5b547c4b17ca774fb5aa2da40c4c9b946575
zqqiang <qiangzhaoqing@gmail.com>
2018-12-06 08:09:45 -0800

    update leetcode

--- src/cpp/leetcode/leetcode.cpp
+++ src/cpp/leetcode/leetcode.cpp
@@ -192,3 +192,124 @@ class Solution
         return vector<int>{minx, miny};
     }
 };
+
+/*
+443. String Compression
+Given an array of characters, compress it in-place.
+
+The length after compression must always be smaller than or equal to the original array.
+
+Every element of the array should be a character (not int) of length 1.
+
+After you are done modifying the input array in-place, return the new length of the array.
+
+ 
+Follow up:
+Could you solve it using only O(1) extra space?
+
+ 
+Example 1:
+
+Input:
+["a","a","b","b","c","c","c"]
+
+Output:
+Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+
+Explanation:
+"aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by "c3".
+ 
+
+Example 2:
+
+Input:
+["a"]
+
+Output:
+Return 1, and the first 1 characters of the input array should be: ["a"]
+
+Explanation:
+Nothing is replaced.
+ 
+
+Example 3:
+
+Input:
+["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+
+Output:
+Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+
+Explanation:
+Since the character "a" does not repeat, it is not compressed. "bbbbbbbbbbbb" is replaced by "b12".
+Notice each digit has it's own entry in the array.
+ 
+
+Note:
+
+All characters have an ASCII value in [35, 126].
+1 <= len(chars) <= 1000.
+*/
+
+class Solution
+{
+  public:
+    int compress(vector<char> &chars)
+    {
+        int total = 0;
+        std::vector<char>::iterator cur_it = chars.begin();
+
+        for (std::vector<char>::iterator it = chars.begin(); it != chars.end();)
+        {
+            char last = *it;
+            int count = 0;
+            total++;
+            for (; it != chars.end() && *it == last; it++)
+            {
+                count++;
+            }
+            if (count != 1)
+            {
+                *cur_it++ = last;
+                std::string count_str = std::to_string(count);
+                for (int i = 0; i < count_str.size(); i++)
+                {
+                    *cur_it++ = count_str[i];
+                }
+                total += count_str.size();
+            }
+            else
+            {
+                *cur_it++ = last;
+            }
+        }
+        return total;
+    }
+};
+
+/*
+Lintcode => 213. String Compression
+Description
+Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2b1c5a3.
+
+If the "compressed" string would not become smaller than the original string, your method should return the original string.
+
+You can assume the string has only upper and lower case letters (a-z).
+
+Example
+str=aabcccccaaa return a2b1c5a3
+str=aabbcc return aabbcc
+str=aaaa return a4
+*/
+class Solution
+{
+  public:
+    /**
+     * @param originalString: a string
+     * @return: a compressed string
+     */
+    string compress(string &originalString)
+    {
+        // write your code here
+    }
+};
