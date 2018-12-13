ff6a06c858bba7b6f2273f00e603e7acf816113e
zqqiang <qiangzhaoqing@gmail.com>
2018-12-13 09:45:07 -0800

    update leetcode

--- src/cpp/leetcode/leetcode.cpp
+++ src/cpp/leetcode/leetcode.cpp
@@ -692,3 +692,79 @@ class Solution
         return true;
     }
 };
+/*
+146. LRU Cache (todos)
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+// capacity
+LRUCache cache = new LRUCache( 2 );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);    // returns 1
+cache.put(3, 3); // evicts key 2
+cache.get(2);    // returns -1 (not found)
+cache.put(4, 4); // evicts key 1
+cache.get(1);    // returns -1 (not found)
+cache.get(3);    // returns 3
+cache.get(4);    // returns 4
+* /
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+
+class LRUCache
+{
+  private:
+    std::map<int, int> m;
+    std::vector<int> vkey;
+    int count;
+    int cap;
+
+  public:
+    LRUCache(int capacity)
+    {
+        this->cap = capacity;
+        this->count = 0;
+    }
+
+    int get(int key)
+    {
+        if (this->m.find(key) == this->m.end())
+        {
+            return -1;
+        }
+        else
+        {
+            return this->m[key];
+        }
+    }
+
+    void put(int key, int value)
+    {
+        if (this->count == this->cap)
+        {
+            int rkey = this->vkey.back();
+            this->m.erase(rkey);
+            this->vkey.pop_back();
+        }
+        if (this->m.find(key) == this->m.end())
+        {
+            this->m[key] = value;
+            this->count++;
+            this->vkey.push_back(key);
+        }
+    }
+};
