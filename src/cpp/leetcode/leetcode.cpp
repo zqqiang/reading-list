7d2f35114f8a4154c040f7038ef35c0fb11ee928
zqqiang <qiangzhaoqing@gmail.com>
2018-12-16 11:50:07 -0800

    update leetcode

--- src/cpp/leetcode/leetcode.cpp
+++ src/cpp/leetcode/leetcode.cpp
@@ -818,3 +818,60 @@ class Solution
         return this->diameter - 1;
     }
 };
+/*
+213. House Robber II (todo)
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed. 
+All houses at this place are arranged in a circle. 
+That means the first house is the neighbor of the last one. 
+Meanwhile, adjacent houses have security system connected 
+and it will automatically contact the police 
+if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, 
+determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+             because they are adjacent houses.
+Example 2:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+*/
+
+class Solution
+{
+  public:
+    int rob(vector<int> &nums)
+    {
+        if (nums.size() == 0)
+        {
+            return 0;
+        }
+        if (nums.size() == 1)
+        {
+            return nums[0];
+        }
+
+        return max(robRange(nums, 0, nums.size() - 1), // Include the first one of nums without the last one.
+                   robRange(nums, 1, nums.size()));    // Include the last one of nums without the first one.
+    }
+
+    int robRange(vector<int> &nums, int start, int end)
+    {
+        int num_i = nums[start], num_i_1 = 0, num_i_2 = 0;
+        for (int i = start + 1; i < end; ++i)
+        {
+            num_i_2 = num_i_1;
+            num_i_1 = num_i;
+            num_i = max(nums[i] + num_i_2, num_i_1);
+        }
+        return num_i;
+    }
+};
