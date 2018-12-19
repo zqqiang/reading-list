2eb53c49d12bb131001cd95738b4bc275f5f66f5
zqqiang <qiangzhaoqing@gmail.com>
2018-12-18 22:09:48 -0800

    update leetcode

--- src/cpp/leetcode/leetcode.cpp
+++ src/cpp/leetcode/leetcode.cpp
@@ -954,36 +954,33 @@ class Solution
   public:
     bool wordBreak(string s, vector<string> &wordDict)
     {
-        for (std::string::iterator it = s.begin(); it != s.end();)
-        {
-            bool find_dic = false;
-            for (std::vector<string>::iterator it_dic = wordDict.begin(); it_dic != wordDict.end(); it_dic++)
-            {
-                bool find = true;
-                for (std::string::iterator d = it_dic->begin(); d != it_dic->end(); d++)
-                {
-                    if (*it == *d)
-                    {
-                        it++;
-                        d++;
-                    }
-                    else
-                    {
-                        find = false;
-                        break;
-                    }
-                }
-                if (find)
-                {
-                    find_dic = true;
-                    break;
-                }
-            }
-            if (!find_dic)
-            {
-                return false;
-            }
-        }
-        return true;
+        // Mark evert word as breakable.
+        for (const string &word : wordDict)
+            mem_.emplace(word, true);
+
+        // Query the answer of the original string.
+        return wordBreak(s);
     }
+
+    bool wordBreak(const string &s)
+    {
+        // In memory, directly return.
+        if (mem_.count(s))
+            return mem_[s];
+
+        // Try every break point.
+        for (int j = 1; j < s.length(); j++)
+        {
+            auto it = mem_.find(s.substr(j));
+            // Find the solution for s.
+            if (it != mem_.end() && it->second && wordBreak(s.substr(0, j)))
+                return mem_[s] = true;
+        }
+
+        // No solution for s, memorize and return.
+        return mem_[s] = false;
+    }
+
+  private:
+    unordered_map<string, bool> mem_;
 };
