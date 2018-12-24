733f61b5aead5e94d9de2fffce94eba842102a72
zqqiang <qiangzhaoqing@gmail.com>
2018-12-24 15:29:23 -0800

    update leetcode

--- src/cpp/leetcode/leetcode.cpp
+++ src/cpp/leetcode/leetcode.cpp
@@ -1536,5 +1536,65 @@ class Solution
   public:
     int findPeakElement(vector<int> &nums)
     {
+        int len = nums.size();
+        for (int i = 1; i < len - 1; ++i)
+        {
+            if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1])
+            {
+                return i;
+            }
+        }
+        return nums[0] > nums[len - 1] ? 0 : len - 1;
+    }
+};
+/*
+75. Find Peak Element
+Description
+There is an integer array which has the following features:
+
+The numbers in adjacent positions are different.
+A[0] < A[1] && A[A.length - 2] > A[A.length - 1].
+We define a position P is a peak if:
+
+A[P] > A[P-1] && A[P] > A[P+1]
+Find a peak element in this array. Return the index of the peak.
+
+It's guaranteed the array has at least one peak.
+The array may contain multiple peeks, find any of them.
+The array has at least 3 numbers in it.
+Have you met this question in a real interview?  
+Example
+Given [1, 2, 1, 3, 4, 5, 7, 6]
+
+Return index 1 (which is number 2) or 6 (which is number 7)
+
+Challenge
+Time complexity O(logN)
+*/
+class Solution
+{
+  public:
+    int findPeakElement(vector<int> &nums)
+    {
+        int l = 1;
+        int r = nums.size();
+
+        while (l <= r)
+        {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+            {
+                return mid;
+            }
+            if (nums[mid] > nums[mid - 1])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+        return -1;
     }
 };
