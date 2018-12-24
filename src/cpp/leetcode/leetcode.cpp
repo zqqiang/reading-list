c443142ae8250f5bd63ee2ea8bc48ba0bf431ebd
zqqiang <qiangzhaoqing@gmail.com>
2018-12-24 10:47:05 -0800

    update leetcode

--- src/cpp/leetcode/leetcode.cpp
+++ src/cpp/leetcode/leetcode.cpp
@@ -1435,3 +1435,74 @@ class Solution
         return q.top();
     }
 };
+/**
+* This reference program is provided by @jiuzhang.com
+* Copyright is reserved. Please indicate the source for forwarding
+*/
+/*
+lintCode solution (todo)
+*/
+class Solution
+{
+  public:
+    /*
+     * param k : description of k
+     * param nums : description of array and index 0 ~ n-1
+     * return: description of return
+     */
+    int kthLargestElement(int k, vector<int> nums)
+    {
+        if (nums.size() == 0 || k < 1 || k > nums.size())
+        {
+            return -1;
+        }
+        return partition(nums, 0, nums.size() - 1, nums.size() - k);
+    }
+
+  private:
+    int partition(vector<int> &nums, int start, int end, int k)
+    {
+        if (start >= end)
+        {
+            return nums[k];
+        }
+
+        int left = start, right = end;
+        int pivot = nums[(start + end) / 2];
+
+        while (left <= right)
+        {
+            while (left <= right && nums[left] < pivot)
+            {
+                left++;
+            }
+            while (left <= right && nums[right] > pivot)
+            {
+                right--;
+            }
+            if (left <= right)
+            {
+                swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        if (k <= right)
+        {
+            return partition(nums, start, right, k);
+        }
+        if (k >= left)
+        {
+            return partition(nums, left, end, k);
+        }
+        return nums[k];
+    }
+
+    void swap(vector<int> &nums, int i, int j)
+    {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+};
