a598c8adb50ac6d78156f965ba2fe721ca9ea671
zqqiang <qiangzhaoqing@gmail.com>
2018-12-24 10:31:02 -0800

    update leetcode

--- src/cpp/leetcode/leetcode.cpp
+++ src/cpp/leetcode/leetcode.cpp
@@ -1402,3 +1402,36 @@ class Solution
         return this->ans;
     }
 };
+/*
+215. Kth Largest Element in an Array
+Find the kth largest element in an unsorted array. 
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+*/
+class Solution
+{
+  public:
+    int findKthLargest(vector<int> &nums, int k)
+    {
+        priority_queue<int> q;
+        for (std::vector<int>::iterator it = nums.begin(); it != nums.end(); it++)
+        {
+            q.push(*it);
+        }
+        for (int i = 1; i < k; ++i)
+        {
+            q.pop();
+        }
+        return q.top();
+    }
+};
