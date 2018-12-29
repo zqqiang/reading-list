b0171888e4e754921d939030944d40c2bef1a631
zqqiang <qiangzhaoqing@gmail.com>
2018-12-29 12:05:16 -0800

    update leetcode

--- src/cpp/leetcode/leetcode.cpp
+++ src/cpp/leetcode/leetcode.cpp
@@ -2064,3 +2064,98 @@ class Solution
         return {left, right};
     }
 };
+/*
+33. Search in Rotated Sorted Array
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+*/
+class Solution
+{
+  private:
+    int find(vector<int> &nums, int l, int r, int target)
+    {
+        int m = l + (r - l) / 2;
+        if (l > r)
+        {
+            return -1;
+        }
+        if (nums[m] == target)
+        {
+            return m;
+        }
+        if (nums[l] <= nums[m])
+        {
+            if (nums[l] <= target && target <= nums[m])
+            {
+                return find(nums, l, m, target);
+            }
+            else
+            {
+                return find(nums, m + 1, r, target);
+            }
+        }
+        else
+        {
+            if (nums[m] <= target && target <= nums[r])
+            {
+                return find(nums, m, r, target);
+            }
+            else
+            {
+                return find(nums, l, m - 1, target);
+            }
+        }
+    }
+
+  public:
+    int search(vector<int> &nums, int target)
+    {
+        return find(nums, 0, nums.size() - 1, target);
+    }
+};
+/*
+23. Merge k Sorted Lists
+Merge k sorted linked lists and return it as one sorted list. 
+Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+  public:
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+    }
+};
