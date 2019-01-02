ded08f871dcaf5a51c56d2048e3362c847737cdf
zqqiang <qiangzhaoqing@gmail.com>
2019-01-02 08:14:30 -0800

    update leetcode

--- doc/interview/golang.md
+++ doc/interview/golang.md
@@ -1,3 +1,4 @@
 # Golang interview Questions
 
 1. [toptal-go-interview-questions](https://www.toptal.com/go/interview-questions) - [stack]()
+1. [Top 20 GO Programming (Golang) Interview Questions & Answers](https://career.guru99.com/top-20-go-programming-interview-questions/)
--- src/cpp/leetcode/leetcode.cpp
+++ src/cpp/leetcode/leetcode.cpp
@@ -2546,3 +2546,45 @@ class Solution
         return true;
     }
 };
+/*
+42. Trapping Rain Water
+Given n non-negative integers representing an elevation map where the width of each bar is 1, 
+compute how much water it is able to trap after raining.
+
+
+The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. 
+In this case, 6 units of rain water (blue section) are being trapped. 
+Thanks Marcos for contributing this image!
+
+Example:
+
+Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+*/
+class Solution
+{
+  public:
+    int trap(vector<int> &height)
+    {
+        int len = height.size();
+        int ans = 0;
+        
+        for (int i = 1; i < len - 1; i++) {
+            int max_left = 0;
+            int max_right = 0;
+            for (int l = 0; l <= i; l++) {
+                if (max_left < height[l]) {
+                    max_left = height[l];
+                }
+            }
+            for (int r = i; r < len; r++) {
+                if (max_right < height[r]) {
+                    max_right = height[r];
+                }
+            }
+            ans += min(max_left, max_right) - height[i];
+        }
+        
+        return ans;
+    }
+};
