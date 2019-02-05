391426bf28f88df03a61580705bae8b3a73036a4
zqqiang <qiangzhaoqing@gmail.com>
2019-02-05 08:02:44 -0800

    update leetcode

--- src/cpp/leetcode/linkedList.cpp
+++ src/cpp/leetcode/linkedList.cpp
@@ -1,5 +1,4 @@
 /*
-450. Reverse Nodes in k-Group
 53. Reverse Words in a String
 */
 
@@ -284,5 +283,70 @@ public:
     bool isPalindrome(ListNode* head) {
         header = head;
         return helper(head);
+    }
+};
+/*
+25. Reverse Nodes in k-Group
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. 
+If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+Example:
+
+Given this linked list: 1->2->3->4->5
+
+For k = 2, you should return: 2->1->4->3->5
+
+For k = 3, you should return: 3->2->1->4->5
+
+Note:
+
+Only constant extra memory is allowed.
+You may not alter the values in the list's nodes, only nodes itself may be changed.
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// not working
+class Solution {
+private:
+    ListNode* helper (ListNode* start, ListNode* end) {
+        ListNode* prev = NULL;
+        ListNode* curr = start;
+        while (NULL != curr && curr != end) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* curr = head;
+        ListNode* ans = NULL;
+        while (1) {
+            ListNode* start = curr;
+            int step = k;
+            while (curr && step) {
+                curr = curr->next;
+                step--;
+            }
+            if (curr) {
+                curr = helper(start, curr);
+                if (NULL == ans) {
+                    ans = curr;
+                }
+            } else {
+                break;
+            }
+        }
+        return ans;
     }
 };
