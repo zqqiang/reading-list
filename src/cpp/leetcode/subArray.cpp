54b2a97588bf9f5eb08ee6e9b0ed6aa400fc1d82
zqqiang <qiangzhaoqing@gmail.com>
2019-02-07 08:58:29 -0800

    update leetcode

--- src/cpp/leetcode/subArray.cpp
+++ src/cpp/leetcode/subArray.cpp
@@ -1,7 +1,6 @@
 /*
 621. Maximum Subarray V
 620. Maximum Subarray IV
-617. Maximum Average Subarray II
 402. Continuous Subarray Sum
 151. Best Time to Buy and Sell Stock III 
 43. Maximum Subarray III
@@ -66,5 +65,92 @@ public:
             ans = std::max(sum, ans);
         }
         return ans/k;
+    }
+};
+/*
+LintCode
+617. Maximum Average Subarray II
+Given an array with positive and negative numbers, 
+find the maximum average subarray which length should be greater or equal to given length k.
+
+It's guaranteed that the size of the array is greater or equal to k.
+Example
+Given nums = [1, 12, -5, -6, 50, 3], k = 3
+
+Return 15.667 // (-6 + 50 + 3) / 3 = 15.667
+*/
+/**
+* This reference program is provided by @jiuzhang.com
+* Copyright is reserved. Please indicate the source for forwarding
+*/
+// todo
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        double l = INT_MAX, r = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            l = min(l, (double)nums[i]);
+            r = max(r, (double)nums[i]);
+        }
+        vector<double> sumNums(n + 1);
+        sumNums[0] = 0;
+        while (r - l > 1e-6) {
+            double mid = (l + r) / 2;
+            for (int i = 0; i < n; i++) {
+                sumNums[i + 1] = sumNums[i] + nums[i] - mid;
+            }
+            double preMin = 0;
+            double sumMax = INT_MIN;
+            for (int i = k; i <= n; i++) {
+                sumMax = max(sumMax, sumNums[i] - preMin);
+                preMin = min(preMin, sumNums[i - k + 1]);
+            }
+            if (sumMax >= 0) {
+                l = mid;
+            }
+            else {
+                r = mid;
+            }
+        }
+        return l;
+    }
+};
+/*
+LintCode
+42. Maximum Subarray II
+Given an array of integers, find two non-overlapping subarrays which have the largest sum.
+The number in each subarray should be contiguous.
+Return the largest sum.
+
+The subarray should contain at least one number
+Example
+Example 1:
+
+Input:
+[1, 3, -1, 2, -1, 2]
+Output:
+7
+Explanation:
+the two subarrays are [1, 3] and [2, -1, 2] or [1, 3, -1, 2] and [2].
+Example 2:
+
+Input:
+[5,4]
+Output:
+9
+Explanation:
+the two subarrays are [5] and [4].
+Challenge
+Can you do it in time complexity O(n) ?
+*/
+class Solution {
+public:
+    /*
+     * @param nums: A list of integers
+     * @return: An integer denotes the sum of max two non-overlapping subarrays
+     */
+    int maxTwoSubArrays(vector<int> &nums) {
+        // write your code here
     }
 };
