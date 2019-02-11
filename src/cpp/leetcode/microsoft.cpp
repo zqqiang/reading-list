9b2464109bfbf36f618dab983d96f63c513c2eda
zqqiang <qiangzhaoqing@gmail.com>
2019-02-10 16:39:28 -0800

    update leetcode

--- src/cpp/leetcode/microsoft.cpp
+++ src/cpp/leetcode/microsoft.cpp
@@ -263,6 +263,27 @@ A solution set is:
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
+        vector<vector<int>> ans;
+        if (nums.size() == 0) return ans;
+        if (nums.size() == 1) return ans;
+        std::sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 2; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            for(int j = i + 1, k = nums.size() - 1; j < k;) {
+                if(nums[i] + nums[j] + nums[k] == 0) {
+                    ans.push_back(vector<int>{nums[i], nums[j], nums[k]});
+                    j++;k--;
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) k--;
+                } else if (nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return ans;
     }
 };
