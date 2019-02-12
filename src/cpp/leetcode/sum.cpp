df035cfe052a5fdd81ddcde0f7ceff656db84b9e
zqqiang <qiangzhaoqing@gmail.com>
2019-02-12 07:47:55 -0800

    update leetcode

--- src/cpp/leetcode/sum.cpp
+++ src/cpp/leetcode/sum.cpp
@@ -1,6 +1,5 @@
 /*
 382. Triangle Count
-59. 3Sum Closest
 58. 4Sum
 56. Two Sum
 */
@@ -43,6 +42,42 @@ public:
                     j++;
                 } else {
                     k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
+/*
+16. 3Sum Closest
+Given an array nums of n integers and an integer target, 
+find three integers in nums such that the sum is closest to target. 
+Return the sum of the three integers. 
+You may assume that each input would have exactly one solution.
+
+Example:
+
+Given array nums = [-1, 2, 1, -4], and target = 1.
+
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+*/
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int min = INT_MAX;
+        int ans = 0;
+        std::sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 2; i++) {
+            for (int j = i + 1, k = nums.size() - 1; j < k;) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (abs(sum - target) < min) {
+                    min = abs(sum - target);
+                    ans = sum;
+                }
+                if (sum - target > 0) {
+                    k--;
+                } else {
+                    j++;
                 }
             }
         }
