46ef715af23d30c282b98703098f1a58fc78d9d1
zqqiang <qiangzhaoqing@gmail.com>
2019-02-22 09:14:12 -0800

    update leetcode

--- src/cpp/leetcode/190.reverse-bits.cpp
+++ src/cpp/leetcode/190.reverse-bits.cpp
@@ -58,6 +58,14 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        
+        uint32_t ans = 0;
+        int count = 32;
+        while(count) {
+            ans *= 2;
+            ans += n & 1;
+            n /= 2;
+            count--;
+        }
+        return ans;
     }
 };
