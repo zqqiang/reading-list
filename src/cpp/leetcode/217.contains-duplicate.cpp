e8a93284e4c2018693a4da47391371e04febf3f7
zqqiang <qiangzhaoqing@gmail.com>
2019-02-26 09:05:30 -0800

    update leetcode

---
+++ src/cpp/leetcode/217.contains-duplicate.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=217 lang=cpp
+ *
+ * [217] Contains Duplicate
+ *
+ * https://leetcode.com/problems/contains-duplicate/description/
+ *
+ * algorithms
+ * Easy (50.66%)
+ * Total Accepted:    301.1K
+ * Total Submissions: 593.5K
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * Given an array of integers, find if the array contains any duplicates.
+ * 
+ * Your function should return true if any value appears at least twice in the
+ * array, and it should return false if every element is distinct.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,3,1]
+ * Output: true
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,3,4]
+ * Output: false
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ * 
+ */
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        if(0 == nums.size()) return false;
+        vector<int> ans(nums.size(), 32767);
+        for(int i = 0; i < nums.size(); i++) {
+            if(ans[nums[i]%nums.size()] == nums[i]) {
+                return true;
+            }
+            ans[nums[i]%nums.size()] = nums[i];
+        }
+        return false;
+    }
+};
---
+++ src/cpp/leetcode/219.contains-duplicate-ii.cpp
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=219 lang=cpp
+ *
+ * [219] Contains Duplicate II
+ *
+ * https://leetcode.com/problems/contains-duplicate-ii/description/
+ *
+ * algorithms
+ * Easy (34.62%)
+ * Total Accepted:    183K
+ * Total Submissions: 527.9K
+ * Testcase Example:  '[1,2,3,1]\n3'
+ *
+ * Given an array of integers and an integer k, find out whether there are two
+ * distinct indices i and j in the array such that nums[i] = nums[j] and the
+ * absolute difference between i and j is at most k.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,2,3,1], k = 3
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1,0,1,1], k = 1
+ * Output: true
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [1,2,3,1,2,3], k = 2
+ * Output: false
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> s;
+        for (auto i = 0; i < nums.size(); ++i) {
+            if (i > k) s.erase(nums[i - k - 1]);
+            if (!s.insert(nums[i]).second) return true;
+        }
+        return false;
+    }
+};
+
