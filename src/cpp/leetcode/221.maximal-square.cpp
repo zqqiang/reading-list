279a7b7c4f36606b10e5d4280549e6e31ad0f968
zqqiang <qiangzhaoqing@gmail.com>
2019-02-27 09:30:49 -0800

    update leetcode

---
+++ src/cpp/leetcode/221.maximal-square.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=221 lang=cpp
+ *
+ * [221] Maximal Square
+ *
+ * https://leetcode.com/problems/maximal-square/description/
+ *
+ * algorithms
+ * Medium (32.16%)
+ * Total Accepted:    118.2K
+ * Total Submissions: 366.9K
+ * Testcase Example:  '[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]'
+ *
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest square
+ * containing only 1's and return its area.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 
+ * 
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ * 
+ * Output: 4
+ * 
+ */
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        
+    }
+};
+
---
+++ src/cpp/leetcode/415.add-strings.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=415 lang=cpp
+ *
+ * [415] Add Strings
+ *
+ * https://leetcode.com/problems/add-strings/description/
+ *
+ * algorithms
+ * Easy (42.97%)
+ * Total Accepted:    84.1K
+ * Total Submissions: 195.6K
+ * Testcase Example:  '"0"\n"0"'
+ *
+ * Given two non-negative integers num1 and num2 represented as string, return
+ * the sum of num1 and num2.
+ * 
+ * Note:
+ * 
+ * The length of both num1 and num2 is < 5100.
+ * Both num1 and num2 contains only digits 0-9.
+ * Both num1 and num2 does not contain any leading zero.
+ * You must not use any built-in BigInteger library or convert the inputs to
+ * integer directly.
+ * 
+ * 
+ */
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        string ans;
+        int carry = 0;
+        for(int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0 || carry > 0; i--, j--) {
+            int x = i < 0 ? 0 : num1[i] - '0';
+            int y = j < 0 ? 0 : num2[j] - '0';
+            int sum = x + y + carry;
+            ans.append(to_string(sum%10));
+            carry  = sum/10;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
