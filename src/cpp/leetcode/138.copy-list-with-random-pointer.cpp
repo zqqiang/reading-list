bfb86baa3c0c057301a2b0f78fe8399a2eb2986b
zqqiang <qiangzhaoqing@gmail.com>
2019-03-16 14:55:18 -0700

    update leetcode

---
+++ src/cpp/leetcode/138.copy-list-with-random-pointer.cpp
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=138 lang=cpp
+ *
+ * [138] Copy List with Random Pointer
+ *
+ * https://leetcode.com/problems/copy-list-with-random-pointer/description/
+ *
+ * algorithms
+ * Medium (25.63%)
+ * Total Accepted:    227.4K
+ * Total Submissions: 875.8K
+ * Testcase Example:  '{"$id":"1","next":{"$id":"2","next":null,"random":{"$ref":"2"},"val":2},"random":{"$ref":"2"},"val":1}'
+ *
+ * A linked list is given such that each node contains an additional random
+ * pointer which could point to any node in the list or null.
+ * 
+ * Return a deep copy of the list.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input:
+ * 
+ * {"$id":"1","next":{"$id":"2","next":null,"random":{"$ref":"2"},"val":2},"random":{"$ref":"2"},"val":1}
+ * 
+ * Explanation:
+ * Node 1's value is 1, both of its next and random pointer points to Node 2.
+ * Node 2's value is 2, its next pointer points to null and its random pointer
+ * points to itself.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * You must return the copy of the given head as a reference to the cloned
+ * list.
+ * 
+ */
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node() {}
+
+    Node(int _val, Node* _next, Node* _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        
+    }
+};
+
---
+++ src/cpp/leetcode/78.subsets.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=78 lang=cpp
+ *
+ * [78] Subsets
+ *
+ * https://leetcode.com/problems/subsets/description/
+ *
+ * algorithms
+ * Medium (50.73%)
+ * Total Accepted:    336.9K
+ * Total Submissions: 656.6K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a set of distinct integers, nums, return all possible subsets (the
+ * power set).
+ * 
+ * Note: The solution set must not contain duplicate subsets.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: nums = [1,2,3]
+ * Output:
+ * [
+ * ⁠ [3],
+ * [1],
+ * [2],
+ * [1,2,3],
+ * [1,3],
+ * [2,3],
+ * [1,2],
+ * []
+ * ]
+ * 
+ */
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+    }
+};
+
