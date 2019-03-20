32bba162d58e1b8ea0bb31721a3ab3b5c6b45076
zqqiang <qiangzhaoqing@gmail.com>
2019-03-20 09:31:40 -0700

    update leetcode

--- src/cpp/leetcode/78.subsets.cpp
+++ src/cpp/leetcode/78.subsets.cpp
@@ -33,22 +33,10 @@
  * ]
  * 
  */
-class Solution
-{
-  public:
-    vector<vector<int>> subsets(vector<int> &nums)
-    {
-        vector<vector<int>> ans{vector<int>{}};
-        for (int i = 0; i < nums.size(); i++)
-        {
-            int size = ans.size();
-            for (int j = 0; j < size; j++)
-            {
-                vector<int> subset{ans[j]};
-                subset.push_back(nums[i]);
-                ans.push_back(subset);
-            }
-        }
-        return ans;
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
     }
 };
+
