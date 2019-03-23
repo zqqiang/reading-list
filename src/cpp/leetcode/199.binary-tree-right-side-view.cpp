e98202bb0f1cac74b745f6c4d556a9c1571f556e
zqqiang <qiangzhaoqing@gmail.com>
2019-03-23 11:23:18 -0700

    update leetcode

---
+++ src/cpp/leetcode/199.binary-tree-right-side-view.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=199 lang=cpp
+ *
+ * [199] Binary Tree Right Side View
+ *
+ * https://leetcode.com/problems/binary-tree-right-side-view/description/
+ *
+ * algorithms
+ * Medium (46.44%)
+ * Total Accepted:    155.3K
+ * Total Submissions: 331.1K
+ * Testcase Example:  '[1,2,3,null,5,null,4]'
+ *
+ * Given a binary tree, imagine yourself standing on the right side of it,
+ * return the values of the nodes you can see ordered from top to bottom.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3,null,5,null,4]
+ * Output: [1, 3, 4]
+ * Explanation:
+ * 
+ * ⁠  1            <---
+ * ⁠/   \
+ * 2     3         <---
+ * ⁠\     \
+ * ⁠ 5     4       <---
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        
+    }
+};
+
