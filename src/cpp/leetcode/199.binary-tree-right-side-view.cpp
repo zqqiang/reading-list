f9dfec24f48e578699fd48c6a829c3c7cec23c14
zqqiang <qiangzhaoqing@gmail.com>
2019-03-23 12:19:25 -0700

    update leetcode

--- src/cpp/leetcode/199.binary-tree-right-side-view.cpp
+++ src/cpp/leetcode/199.binary-tree-right-side-view.cpp
@@ -40,7 +40,26 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        
+        queue<TreeNode*> q;
+        vector<int> ans;
+        if(!root) {
+            return ans;
+        }
+        q.push(root);
+        while(!q.empty()) {
+            ans.push_back(q.back()->val);
+            for(int i = q.size(); i > 0; i--) {
+                TreeNode* node =q.front();
+                q.pop();
+                if(node->left) {
+                    q.push(node->left);
+                }
+                if(node->right) {
+                    q.push(node->right);
+                }
+            }
+        }
+        return ans;
     }
 };
 
