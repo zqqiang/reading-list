417577405b7b6ed6d37941d6e665b5cc2d6d64c4
zqqiang <qiangzhaoqing@gmail.com>
2019-03-25 07:19:02 -0700

    update leetcode

---
+++ src/cpp/leetcode/200.number-of-islands.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=200 lang=cpp
+ *
+ * [200] Number of Islands
+ *
+ * https://leetcode.com/problems/number-of-islands/description/
+ *
+ * algorithms
+ * Medium (40.13%)
+ * Total Accepted:    318.2K
+ * Total Submissions: 782.7K
+ * Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]'
+ *
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of
+ * islands. An island is surrounded by water and is formed by connecting
+ * adjacent lands horizontally or vertically. You may assume all four edges of
+ * the grid are all surrounded by water.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ * 
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ * 
+ * Output: 3
+ * 
+ */
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        
+    }
+};
+
