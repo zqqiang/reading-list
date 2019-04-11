c31ba39ec1c935b19c83e3cf4c5b5ee8e080dd2e
zqqiang <qiangzhaoqing@gmail.com>
2019-04-11 09:14:27 -0700

    update leetcode

---
+++ src/cpp/leetcode/307.range-sum-query-mutable.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=307 lang=cpp
+ *
+ * [307] Range Sum Query - Mutable
+ *
+ * https://leetcode.com/problems/range-sum-query-mutable/description/
+ *
+ * algorithms
+ * Medium (27.31%)
+ * Total Accepted:    68.5K
+ * Total Submissions: 244.2K
+ * Testcase Example:  '["NumArray","sumRange","update","sumRange"]\n[[[1,3,5]],[0,2],[1,2],[0,2]]'
+ *
+ * Given an integer array nums, find the sum of the elements between indices i
+ * and j (i ≤ j), inclusive.
+ * 
+ * The update(i, val) function modifies nums by updating the element at index i
+ * to val.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [1, 3, 5]
+ * 
+ * sumRange(0, 2) -> 9
+ * update(1, 2)
+ * sumRange(0, 2) -> 8
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * The array is only modifiable by the update function.
+ * You may assume the number of calls to update and sumRange function is
+ * distributed evenly.
+ * 
+ * 
+ */
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+        
+    }
+    
+    void update(int i, int val) {
+        
+    }
+    
+    int sumRange(int i, int j) {
+        
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(i,val);
+ * int param_2 = obj->sumRange(i,j);
+ */
+
