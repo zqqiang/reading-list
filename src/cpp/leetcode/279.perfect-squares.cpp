c6cfbf6db5a0d6aa5d0eb3a3678a978d70ec5efe
zqqiang <qiangzhaoqing@gmail.com>
2019-06-06 09:21:50 -0700

    update leetcode

---
+++ src/cpp/leetcode/279.perfect-squares.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=279 lang=cpp
+ *
+ * [279] Perfect Squares
+ */
+class Solution 
+{
+public:
+    int numSquares(int n) 
+    {
+        if(n <= 0) return 0;
+
+        static vector<int> cntPerfectSquares({0});
+
+        while(cntPerfectSquares.size() <= n) {
+            int m = cntPerfectSquares.size();
+            int cntSquares = INT_MAX;
+            for(int i = 1; i*i <= m; i++) {
+                cntSquares = min(cntSquares, cntPerfectSquares[m - i*i] + 1);
+            }
+            cntPerfectSquares.push_back(cntSquares);
+        }
+
+        return cntPerfectSquares[n];
+    }
+};
+
