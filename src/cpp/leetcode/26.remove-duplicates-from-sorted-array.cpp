df5661179590d2818e5717c232c7d670fa17891c
zqqiang <qiangzhaoqing@gmail.com>
2019-07-09 09:14:07 -0700

    update leetcode

---
+++ src/cpp/leetcode/26.remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=26 lang=cpp
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int count = 0;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] == nums[i-1]) count++;
+            else nums[i-count] = nums[i];
+        }
+        return nums.size()-count;
+    }
+};
+
