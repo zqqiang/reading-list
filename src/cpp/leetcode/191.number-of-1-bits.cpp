1e806ba866b50ee0c1c0eec925dad18cc6e37b1e
zqqiang <qiangzhaoqing@gmail.com>
2019-07-19 08:35:50 -0700

    update leetcode

---
+++ src/cpp/leetcode/191.number-of-1-bits.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=191 lang=cpp
+ *
+ * [191] Number of 1 Bits
+ */
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int count = 0;
+
+        while (n)
+        {
+            n &= (n - 1);
+            count++;
+        }
+
+        return count;
+    }
+};
