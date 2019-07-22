12e4ff3decb4a406dfff30652439ef2f81375bcd
zqqiang <qiangzhaoqing@gmail.com>
2019-07-22 09:45:24 -0700

    update leetcode

---
+++ src/cpp/leetcode/338.counting-bits.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=338 lang=cpp
+ *
+ * [338] Counting Bits
+ */
+class Solution
+{
+public:
+    vector<int> countBits(int num)
+    {
+        vector<int> ret(num + 1, 0);
+        for (int i = 1; i <= num; ++i)
+            ret[i] = ret[i & (i - 1)] + 1;
+        return ret;
+    }
+};
