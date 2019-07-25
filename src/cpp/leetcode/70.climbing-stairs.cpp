4da0a20ec2c2cbaff2afb320636185db32a0ceac
zqqiang <qiangzhaoqing@gmail.com>
2019-07-25 08:17:10 -0700

    update leetcode

--- src/cpp/leetcode/70.climbing-stairs.cpp
+++ src/cpp/leetcode/70.climbing-stairs.cpp
@@ -43,14 +43,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(1 == n) return 1;
-        vector<int> dp(n+1, 0);
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        if(n <= 2) return n;
+
+        int one_step = 2;
+        int two_step = 1;
+        int all_step = 0;
+        
+        for(int i = 2; i < n; i++) {
+            all_step = one_step + two_step;
+            two_step = one_step;
+            one_step = all_step;
         }
-        return dp[n];
+        return all_step;
     }
 };
 
