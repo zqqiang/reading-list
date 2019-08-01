e4751efd0ad8c571e1904e1ee8591f35be06357a
zqqiang <qiangzhaoqing@gmail.com>
2019-08-01 08:40:08 -0700

    update leetcode

---
+++ src/cpp/leetcode/139.word-break.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=139 lang=cpp
+ *
+ * [139] Word Break
+ */
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] &&
+                    find(wordDict.begin(), wordDict.end(), s.substr(j, i - j)) != wordDict.end()) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
+
